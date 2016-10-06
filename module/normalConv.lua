8a8858b0c64d93b083b2b10c1d3af7af57c82d84
pby5 <panboyang@126.com>
2016-10-06 17:21:29 +0800

    add ssd code

---
+++ module/normalConv.lua
@@ -0,0 +1,23 @@
+require 'nn'
+require 'cudnn'
+
+do
+  local SpatialConvolution, parent = torch.class('cudnn.normalConv', 'cudnn.SpatialConvolution')  
+  -- override the constructor to have the additional range of initialization
+  function SpatialConvolution:__init(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH, mean, std)
+    parent.__init(self,nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH)                
+    self:reset(mean,std)
+  end
+  -- override the :reset method to use custom weight initialization.        
+  function SpatialConvolution:reset(mean,stdv)        
+    if mean and stdv then
+      self.weight:normal(mean,stdv)
+      self.bias:zero()
+    else
+      stdv = math.sqrt(3/(self.kW*self.kH*self.nInputPlane))   
+      self.weight:uniform(-stdv, stdv)
+      --self.weight:normal(0,1)
+      self.bias:zero()
+    end
+  end
+end
