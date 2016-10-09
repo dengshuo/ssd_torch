32ab3791427789296b81103f909f6eb6d31527f6
Boyang Pan <panboyang@126.com>
2016-10-09 09:09:56 +0800

    Delete load_model.lua

--- load_model.lua
+++
@@ -1,6 +0,0 @@
-require 'loadcaffe'
-require 'torch'
-model = loadcaffe.load('VGG_ILSVRC_16_layers_deploy.prototxt', 'VGG_ILSVRC_16_layers.caffemodel')
-print(model)
-model_path = "./models/VGG/vgg_pretrain.t7"
-torch.save(model_path, model)
